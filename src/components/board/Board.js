5967f75be814bac6691b8b7cd2a60197fb023e22
Orest Matsukh <orest.sd@gmail.com>
2024-08-04 18:56:13 +0300

    add

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,11 +1,23 @@
 
+const boardLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
 function Board() {
-  const square = (el, couples) => <div className={`item ${couples}`} key={el}></div>;
+  const stepSize = 12.5;
+  const positionsFigure = [];
+  const square = (el, couples, squareCoord) => {
+    return (
+      <div className={`item ${couples}`} key={el} onClick={() => {
+        console.log(squareCoord);
+      }}></div>
+    )
+  };
   let allBoard = [];
   let count = 0;
+  let boardNumber = 1;
+  let boardLetterIndex = 0;
 
   for (let i = 0; i < 64; i++) {
-    let couples = i % 2 ? 'item-bg' : '';
+    let couples;
     count = count + 1;
 
     if (count <= 8) {
@@ -16,8 +28,24 @@ function Board() {
       count = 1;
     }
 
-    allBoard.push(square(i, couples));
+    const squareCoord = {
+      letter: boardLetters[boardLetterIndex],
+      number: boardNumber,
+      x: stepSize * boardLetterIndex + 1,
+      y: stepSize * boardNumber
+    }
+
+    if (boardLetterIndex == 7) {
+      boardLetterIndex = 0;
+      boardNumber++;
+    } else {
+      boardLetterIndex++;
+    }
+
+    positionsFigure.push(squareCoord)
+    allBoard.push(square(i, couples, squareCoord));
   }
+
 
   return (
     <div className="board">
